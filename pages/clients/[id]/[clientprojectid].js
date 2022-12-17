2d12ea4370df8f25f3bee5d905b722a082147f67
J. Ring-Sakabe <jordanringsakabe@gmail.com>
2022-12-17 11:53:04 -0700

    completed-next-lesson

--- pages/clients/[id]/[clientprojectid].js
+++ pages/clients/[id]/[clientprojectid].js
@@ -1,6 +1,10 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 const SelectedClientProjectPage = () => {
+  const router = useRouter();
+  console.log(router.query);
+
   return (
     <div>
       <h1>SelectedClientProjectPage</h1>
--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,9 +1,13 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 const ClientProjectsPage = () => {
+  const router = useRouter();
+  console.log(router.query);
+
   return (
     <div>
-      <h1>The Client Projects Page</h1>
+      <h1>The Client's Project's Page</h1>
     </div>
   );
 };
