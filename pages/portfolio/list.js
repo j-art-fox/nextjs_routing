6e22c17e77f9db2447f6c417d7d212011d7ff445
J. Ring-Sakabe <jordanringsakabe@gmail.com>
2022-12-17 11:22:11 -0700

    completed-next-lesson

--- pages/portfolio/list.js
+++ pages/portfolio/list.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 
 export default class ListPage extends Component {
   render() {
-    return <div>ListPage</div>;
+    return (
+      <div>
+        <h1>The List Page</h1>
+      </div>
+    );
   }
 }
