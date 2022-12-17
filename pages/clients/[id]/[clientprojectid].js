812b544ef44f461892fe76d5c8da77ad7d8948ce
J. Ring-Sakabe <jordanringsakabe@gmail.com>
2022-12-17 11:37:59 -0700

    completed-next-lesson

---
+++ pages/clients/[id]/[clientprojectid].js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SelectedClientProjectPage = () => {
+  return (
+    <div>
+      <h1>SelectedClientProjectPage</h1>
+    </div>
+  );
+};
+
+export default SelectedClientProjectPage;
