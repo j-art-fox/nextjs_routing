d46a0001459eb6cce62c730c49e4b568ce67c791
J. Ring-Sakabe <jordanringsakabe@gmail.com>
2022-12-18 12:59:02 -0700

    completed-next-lesson

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Link from "next/link";
 
 const ClientsPage = () => {
+  const clients = [
+    { id: "max", name: "Maximilian" },
+    { id: "manu", name: "Manuel" },
+  ];
   return (
     <div>
       <h1>ClientsPage</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
