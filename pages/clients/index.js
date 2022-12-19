fe5c802ea2f7e59c244712598cb4db8ba412e152
Jordan Ring-Sakabe <jordanringsakabe@gmail.com>
2022-12-19 12:35:38 -0700

    completed-next-lesson

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -12,7 +12,7 @@ const ClientsPage = () => {
       <ul>
         {clients.map((client) => (
           <li key={client.id}>
-            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+            <Link href={{pathname:'clients/[id]', query:{client.id}}}>{client.name}</Link>
           </li>
         ))}
       </ul>
